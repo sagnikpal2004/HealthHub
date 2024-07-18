e876c3805a4db6290233995e4be4b38f1214c856
Sagnik Pal <sagnikpal2004@outlook.com>
2024-07-18 16:00:38 +0530

    Initial commit

---
+++ src/test/java/nic/HealthHub/HealthHubApplicationTests.java
@@ -0,0 +1,13 @@
+package nic.HealthHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
